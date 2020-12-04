6a2318a26ad351fc2ab0eb04319fa02acd169e43
BrandonG2004 <713150@aacps.org>
2020-12-04 16:46:23 +0000

    Random Walker

--- index.java
+++ index.java
@@ -8,9 +8,11 @@ int x = 200;
 int y = 200;
 
 
+
+
 void setup() {
 	size(500, 500);
-  frameRate(10);
+  frameRate(30);
 }
 
 void draw() {
@@ -19,12 +21,63 @@ void draw() {
   rect(x, y, 10, 10);
   
   //Here is where you should create the new rectangles to create the illusion of 'random walking'
+  int r = (int)(Math.random() * 6);
   // You will need to make sure you dont draw more rectangles than the limit of steps
   //You'll need to use the Math.random() to decide how to create randomness in your drawing of rectangles.
   // Keep your canvas and rectangle width and height as I've given you
+    if(r == 0){
+      rect(x,y,10,10);
+      x = x + 0;
+      y = y - 10;
+      steps = steps + 1;
+    }else if(r == 1){
+      rect(x,y,10,10);
+      x = x + 10;
+      y = y + 0;
+      steps = steps + 1;
+    }else if(r == 2){
+      rect(x,y,10,10);
+      x = x + 0;
+      y = y + 10;
+      steps = steps + 1;
+    }else if(r == 3){
+      rect(x,y,10,10);
+      x = x - 10;
+      y = y + 0;
+      steps = steps + 1;
+    }else if(r == 4){
+      rect(x,y,10,10);
+      x = x - 10;
+      y = y - 10;
+      steps = steps + 1;
+    }else if(r == 5){
+      rect(x,y,10,10);
+      x = x + 10;
+      y = y + 10;
+      steps = steps + 1;
+    }
+    if(x > 500){
+      rect(x,y,10,10);
+      x = x - 10;
+     
+    }else if (y > 500){
+      rect(x,y,10,10);
+    
+      y = y - 10;
+    }else if(y == 0){
+      rect(x,y,10,10);
+    
+      y = y + 10;
+    }else if(x == 0){
+      rect(x,y,10,10);
+      x = x + 10;
+      
+    }
+      
+
 
  
-  
+    println(n);
     println("drew rect at: " + x + ", " + y);
     println("Steps taken: " + steps);
 }
